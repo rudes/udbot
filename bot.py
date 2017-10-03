b30b82273b0935f8c9df47a5733d6325eb1e8c8f
rudes <jacobriddle94@gmail.com>
2017-10-03 11:52:20 -0400

    adding urandom command

--- bot.py
+++ bot.py
@@ -21,27 +21,40 @@ async def on_message(m):
     if m.content.startswith(prefix+'ud'):
         await ud_handler(m)
         return
+    if m.content.startswith(prefix+'urandom'):
+        await ud_random_handler(m)
+        return
 
 async def ud_handler(m):
     mess = m.content.split(" ")
     search = " ".join(mess[1:])
     defs = ud.define(search)
-    ud_url = "http://www.urbandictionary.com/define.php?term="+search
-    if len(mess) > 2:
-        ud_url = "http://www.urbandictionary.com/define.php?term="+"+".join(search.split(" "))
     if not defs:
         logging.info('ud_handler,{},{},{},failed'.format(m.author.name, m.server.name, search))
         await client.send_message(m.channel, "I got nothing")
         return
+    ud_url = "http://www.urbandictionary.com/define.php?term="+search
+    if len(mess) > 2:
+        ud_url = "http://www.urbandictionary.com/define.php?term="+"+".join(search.split(" "))
+    logging.info('ud_handler,{},{},{},success'.format(m.author.name, m.server.name, search))
+    await ud_response(defs[0])
+
+async def ud_random_handler(m):
+    logging.info('ud_random_handler,{},{},{},success'.format(m.author.name, m.server.name, search))
+    await ud_response(ud.random()[0])
+
+async def ud_response(d):
+    ud_url = "http://www.urbandictionary.com/define.php?term="+d.word
+    if len(mess) > 2:
+        ud_url = "http://www.urbandictionary.com/define.php?term="+"+".join(d.word.split(" "))
     em = discord.Embed()
     em.color = discord.Colour(randint(0, 16777215))
-    em.set_author(name=defs[0].word, url=ud_url)
-    em.description = defs[0].definition
-    if defs[0].example:
-        em.add_field(name="Example", value=defs[0].example, inline=False)
-    em.add_field(name="Up Votes", value=str(defs[0].upvotes))
-    em.add_field(name="Down Votes", value=str(defs[0].downvotes))
-    logging.info('ud_handler,{},{},{},success'.format(m.author.name, m.server.name, search))
+    em.set_author(name=d.word, url=ud_url)
+    em.description = d.definition
+    if d.example:
+        em.add_field(name="Example", value=d.example, inline=False)
+    em.add_field(name="Up Votes", value=str(d.upvotes))
+    em.add_field(name="Down Votes", value=str(d.downvotes))
     await client.send_message(m.channel, embed=em)
 
 client.run(str(os.environ['DISCORD_BOTKEY']))
